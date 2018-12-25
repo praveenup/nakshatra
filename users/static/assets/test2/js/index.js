40fd350c64757618a7c981f24fce2652aced9c8a
praveenup <prakum.633@gmail.com>
2018-12-25 14:02:02 +0530

    changes the stars

--- users/static/assets/test2/js/index.js
+++ users/static/assets/test2/js/index.js
@@ -1,136 +1,110 @@
-const DEBUG_MODE = "FALSE";
-
-var divs = [];
-var counter = 0;
-var maxDivs = 100;
-var bodyH, bodyW, words, lingo;
-
-changeWords();
-
-//initially load n amount of particles
-for(i = 0; i < maxDivs/10; i++) {
-	newLingo();
-}
-
-//add additional particles on a set interval with initial delay
-setTimeout(function(){
-	setInterval(newLingo, 25);
-}, 7500);
-
-function newLingo() {
-	var buffer = 0; //as in margins
-	bodyW = document.body.clientWidth;
-	bodyH = document.body.clientHeight;
-	
-	counter++;
-	lingo = getRandomValue(words);
-	
-	//convert less than symbol to HTML compliant
-	var lthan = "<";
-	var regi = new RegExp(lthan, 'g');
-	lingo = lingo.replace(regi, '&lt;');
-	
-	//dynamically add div
-	var div = document.createElement('div');
-	div.id = 'div' + counter;
-	div.classList.add('lingo');
-	div.style.left = getRandomInt(buffer, (bodyW - div.style.width) -buffer) + 'px';
-	div.style.top = getRandomInt(buffer, (bodyH - div.style.height)-buffer) + 'px';
-	div.innerHTML = lingo;
-	
-	//set CSS animation properties
-	div.style.setProperty('--init-x', div.style.left + 'px');
-	div.style.setProperty('--init-y', div.style.top + 'px');
-	var _destZ = getRandomInt(100,600);
-	
-	var quad = getQuandrantForElem(div);
-	if (quad === 1) {
-		div.style.setProperty('--dest-x', -50 + '%');
-		div.style.setProperty('--dest-y', -50 + '%');
-		div.style.setProperty('--dest-z', _destZ + 'px');
-	}
-	if (quad === 2) {
-		div.style.setProperty('--dest-x', 50 + '%');
-		div.style.setProperty('--dest-y', -50 + '%');
-		div.style.setProperty('--dest-z', _destZ + 'px');
-	}
-	if (quad === 3) {
-		div.style.setProperty('--dest-x', -50 + '%');
-		div.style.setProperty('--dest-y', 50 + '%');
-		div.style.setProperty('--dest-z', _destZ + 'px');
-	}
-	if (quad === 4) {
-		div.style.setProperty('--dest-x', 50 + '%');
-		div.style.setProperty('--dest-y', 50 + '%');
-		div.style.setProperty('--dest-z', _destZ + 'px');
-	}
-	
-	//add the div to the document and divs array
-	document.body.appendChild(div);	
-	divs.push(div.id);
-	log('divs count: ' + divs.length);
-	log('width of word ' + lingo.length + ' width of div ' + div.clientWidth + ' height of div ' + div.clientHeight);
-	if (divs.length > maxDivs) {
-		//if the amount of divs is more than the max allowed specified above, delete the most recent first div applied to the document and divs array
-		log('removing element: ' + divs[0]);
-		var elem = document.getElementById(divs[0]);
-		elem.parentNode.removeChild(elem);
-		divs.shift();
-		log('shifted count: ' + divs.length + ' first Index: ' + divs[0]);
-	}
-}
-
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
-}
-
-function getRandomValue(array) {
- return array[Math.floor(Math.random()*array.length)];
-}
-
-// This should be refined and improved for accuracy.
-function getQuandrantForElem(elem) {
-	var result = 0;
-	//the average width to word ratio is 8.75 (based on font-size). All elements (divs) have a height of 19
-	var divDim = {'X' : elem.style.left.slice(0,-2), 'Y' : elem.style.top.slice(0,-2)};
-	var buffer = 50;
-	
-	if (divDim.X >= (bodyW/2) - buffer) {
-		if (divDim.Y >= (bodyH/2)) {
-			result = 4;
+particlesJS("particles-js", {
+	"particles": {
+	  "number": {
+		"value": 355,
+		"density": {
+		  "enable": true,
+		  "value_area": 789.1476416322727
 		}
-		if (divDim.Y <= (bodyH/2)) {
-			result = 2;
+	  },
+	  "color": {
+		"value": "#ffffff"
+	  },
+	  "shape": {
+		"type": "circle",
+		"stroke": {
+		  "width": 0,
+		  "color": "#000000"
+		},
+		"polygon": {
+		  "nb_sides": 5
+		},
+		"image": {
+		  "src": "img/github.svg",
+		  "width": 100,
+		  "height": 100
 		}
-	}
-	
-	if (divDim.X <= (bodyW/2) - buffer) {
-		if (divDim.Y >= (bodyH/2)) {
-			result = 3;
+	  },
+	  "opacity": {
+		"value": 0.48927153781200905,
+		"random": false,
+		"anim": {
+		  "enable": true,
+		  "speed": 0.2,
+		  "opacity_min": 0,
+		  "sync": false
 		}
-		if (divDim.Y <= (bodyH/2)) {
-			result = 1;
+	  },
+	  "size": {
+		"value": 2,
+		"random": true,
+		"anim": {
+		  "enable": true,
+		  "speed": 2,
+		  "size_min": 0,
+		  "sync": false
 		}
-	}
-
-	return result;
-}
-
-function changeWords() {
-	var _index = document.getElementById("thisSelect").value;
-	switch (_index) {
-		case "0":
-			//words = ['function()','Debug.Print()',"for(i=0;i<length;i++)",'var i', 'ASCII', 'Math.random()', 'abs()', 'block', 'bool', 'hasDedidcatedThread()', 'setup.exe', 'var me = new Person()', 'terminate()', 'abort()', 'acos()', 'asin()', 'ceil()', 'floor()', 'isalpha()', 'remove()', 'toupper()', 'modf()', '<html></html>', '$.ajax({})', 'int64', '$query', 'AI', 'crypt()','===', '!==', 'empty()', 'operators', 'threading()', 'session_start', '$_GET["paramater"]', '$POST["password"]', 'hypertext', "color: black;", "font-family: Consolas", 'array_push()', 'array.push', 'array_map()', 'array_diff()', 'base64_decode', 'bcmod', 'bcpow', 'CachingIterator::count', 'new Date()', 'session_encode', 'shuffle()', 'sin', 'sizeOf', 'socket_bind', 'socket_accept', 'socket_sendto', 'DateTime::setDate'];
-			words = ['NIT', 'CALICUT', '2K19',];
-			break;
-		case "1":
-			words = ['0', '1'];
-			break;
-	}
-}
-
-function log(text) {
-	if (DEBUG_MODE === "TRUE") console.log(text);
-}
+	  },
+	  "line_linked": {
+		"enable": false,
+		"distance": 150,
+		"color": "#ffffff",
+		"opacity": 0.4,
+		"width": 1
+	  },
+	  "move": {
+		"enable": true,
+		"speed": 1,
+		"direction": "none",
+		"random": true,
+		"straight": false,
+		"out_mode": "out",
+		"bounce": false,
+		"attract": {
+		  "enable": false,
+		  "rotateX": 600,
+		  "rotateY": 1200
+		}
+	  }
+	},
+	"interactivity": {
+	  "detect_on": "canvas",
+	  "events": {
+		"onhover": {
+		  "enable": true,
+		  "mode": "bubble"
+		},
+		"onclick": {
+		  "enable": true,
+		  "mode": "push"
+		},
+		"resize": true
+	  },
+	  "modes": {
+		"grab": {
+		  "distance": 400,
+		  "line_linked": {
+			"opacity": 1
+		  }
+		},
+		"bubble": {
+		  "distance": 83.91608391608392,
+		  "size": 1,
+		  "duration": 3,
+		  "opacity": 1,
+		  "speed": 3
+		},
+		"repulse": {
+		  "distance": 200,
+		  "duration": 0.4
+		},
+		"push": {
+		  "particles_nb": 4
+		},
+		"remove": {
+		  "particles_nb": 2
+		}
+	  }
+	},
+	"retina_detect": true
+  });
