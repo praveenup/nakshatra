594a820c8d354275a647876bef9aa18eff267199
Praveen Kumar <prakum.633@gmail.com>
2018-12-30 23:42:29 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -81,18 +81,18 @@ DATABASES = {
     'default': {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        # 'ENGINE': 'django.db.backends.mysql',
-        # 'NAME': 'prakumup$nakshatra19',
-        # 'USER': 'prakumup',
-        # 'PASSWORD': 'nakshatra2k19',
-        # 'HOST': 'prakumup.mysql.pythonanywhere-services.com',
-        # 'PORT': '3306',   #my port is 3306
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'nakshatra19',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'NAME': 'prakumup$nakshatra19',
+        'USER': 'prakumup',
+        'PASSWORD': 'nakshatra2k19',
+        'HOST': 'prakumup.mysql.pythonanywhere-services.com',
+        'PORT': '3306',   #my port is 3306
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'nakshatra19',
+#         'USER': 'root',
+#         'PASSWORD': '',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
     }
 }
 
