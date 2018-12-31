af0d9da5446c14194a0ea743106ef50fd03159fb
praveenup <prakum.633@gmail.com>
2018-12-31 08:50:54 +0000

    changes in server

--- events/models.py
+++ events/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from PIL import Image
 from users.models import College
+from django.core.exceptions import ValidationError
+from django.core.validators import FileExtensionValidator
 
 class PhotoRegistration(models.Model):
     college = models.OneToOneField(College, on_delete=models.CASCADE)
@@ -13,15 +15,20 @@ class PhotoRegistration(models.Model):
     def __str__(self):
         template = '{0.college.college_name} {0.student_name}'
         return template.format(self)
-    
+
 
 class UserPhoto(models.Model):
+    def validate_image(fieldfile_obj):
+        filesize = fieldfile_obj.file.size
+        megabyte_limit = 5.0
+        if filesize > megabyte_limit*1024*1024:
+            raise ValidationError("Max file size is %sMB" % str(megabyte_limit))
+
     photo_reg = models.OneToOneField(PhotoRegistration, on_delete=models.CASCADE)
     description = models.TextField()
-    image = models.ImageField(upload_to='photography_pics')
+    image = models.ImageField(upload_to='photography_pics',  validators=[FileExtensionValidator(['jpg','jpeg','png']), validate_image])
 
     def __str__(self):
         return self.photo_reg.email
-
 
 
--- mysite/settings.py
+++ mysite/settings.py
@@ -155,3 +155,7 @@ EMAIL_HOST_PASSWORD = 'nakshatra123'
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
